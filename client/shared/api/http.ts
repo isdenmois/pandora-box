f4d94c0c9e2a48c03749efc441699887b3cecb44
Denis Moiseev <isdenmois@gmail.com>
2025-11-05 20:46:09 +0300

    chore: wretch update

--- client/shared/api/http.ts
+++ client/shared/api/http.ts
@@ -3,7 +3,7 @@ import QueryStringAddon from 'wretch/addons/queryString'
 import { useAuth } from '../lib/auth/auth-state'
 
 export const http = wretch('/api')
-  .addon(QueryStringAddon)
-  .errorType('json')
+  .customError(async (error, response) => ({ ...error, json: await response.json() }))
   .catcher(403, () => useAuth().setUser(null))
   .catcher(401, () => useAuth().setUser(null))
+  .addon(QueryStringAddon)
